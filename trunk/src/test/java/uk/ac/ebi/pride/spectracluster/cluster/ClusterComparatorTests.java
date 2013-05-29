8cf8a8e5146e1a8878c6b751ba4cddee80e9725f
harry.r.wang <harry.r.wang@gmail.com>
2013-05-29 19:55:58 +0000

    unit test for cluster comparator

---
+++ trunk/src/test/java/uk/ac/ebi/pride/spectracluster/cluster/ClusterComparatorTests.java
@@ -0,0 +1,43 @@
+package uk.ac.ebi.pride.spectracluster.cluster;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.ebi.pride.spectracluster.quality.OriginalSignalToNoiseChecker;
+import uk.ac.ebi.pride.spectracluster.quality.SignalToNoiseChecker;
+import uk.ac.ebi.pride.spectracluster.spectrum.IPeptideSpectrumMatch;
+import uk.ac.ebi.pride.spectracluster.util.ParserUtilities;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ * @author Rui Wang
+ * @version $Id$
+ */
+public class ClusterComparatorTests {
+
+    private IPeptideSpectrumMatch[] peptideSpectrumMatches;
+    private ClusterComparator clusterComparator;
+    private OriginalClusterComparator originalClusterComparator;
+
+    @Before
+    public void setUp() throws Exception {
+        clusterComparator = new ClusterComparator(new SignalToNoiseChecker());
+        originalClusterComparator = new OriginalClusterComparator(new OriginalSignalToNoiseChecker());
+
+        URL url = ClusterComparatorTests.class.getClassLoader().getResource("uk/ac/ebi/pride/spectracluster/util/two_spectra.mgf");
+        if (url == null) {
+            throw new IllegalStateException("no file for input found!");
+        }
+        File inputFile = new File(url.toURI());
+        peptideSpectrumMatches = ParserUtilities.readMGFScans(inputFile);
+    }
+
+    @Test
+    public void testClusterComparator() throws Exception {
+        int result = clusterComparator.compare(peptideSpectrumMatches[0].asCluster(), peptideSpectrumMatches[1].asCluster());
+        int originalResult = originalClusterComparator.compare(peptideSpectrumMatches[0].asCluster(), peptideSpectrumMatches[1].asCluster());
+        Assert.assertEquals(result, originalResult);
+    }
+}
