7b6eb074d8f423b919828e854b7cb0edd2f55e6b
harry.r.wang@gmail.com <harry.r.wang@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2013-05-29 19:29:36 +0000

    unit test for signal to noise quality checker

---
+++ trunk/src/test/java/uk/ac/ebi/pride/spectracluster/quality/SignalToNoiseQualityCheckerTests.java
@@ -0,0 +1,44 @@
+package uk.ac.ebi.pride.spectracluster.quality;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.ebi.pride.spectracluster.spectrum.IPeptideSpectrumMatch;
+import uk.ac.ebi.pride.spectracluster.util.ParserUtilities;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ * @author Rui Wang
+ * @version $Id$
+ */
+public class SignalToNoiseQualityCheckerTests {
+    private IPeptideSpectrumMatch[] peptideSpectrumMatches;
+    private QualityScorer originalQualityScorer;
+    private QualityScorer qualityScorer;
+
+
+    @Before
+    public void setUp() throws Exception {
+        originalQualityScorer = new OriginalSignalToNoiseChecker();
+        qualityScorer = new SignalToNoiseChecker();
+
+        URL url = SignalToNoiseQualityCheckerTests.class.getClassLoader().getResource("uk/ac/ebi/pride/spectracluster/util/two_spectra.mgf");
+        if (url == null) {
+            throw new IllegalStateException("no file for input found!");
+        }
+        File inputFile = new File(url.toURI());
+        peptideSpectrumMatches = ParserUtilities.readMGFScans(inputFile);
+    }
+
+    @Test
+    public void testSignalToNoiseQualityChecker() throws Exception {
+        for (IPeptideSpectrumMatch peptideSpectrumMatch : peptideSpectrumMatches) {
+
+            double originalScore = originalQualityScorer.calculateQualityScore(peptideSpectrumMatch);
+            double score = qualityScorer.calculateQualityScore(peptideSpectrumMatch);
+            Assert.assertEquals(score, originalScore, 0.1);
+        }
+    }
+}
