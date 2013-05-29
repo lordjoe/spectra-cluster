65561ba75f712060309bc2c5d2c8641978034b16
attilacsordas <attilacsordas@332a39bd-9e54-5415-0f82-69358dc792e7>
2013-05-29 20:20:23 +0000

    working multi similarity test and comparing all the cluster spectra members against each other per cluster

--- trunk/src/test/java/uk/ac/ebi/pride/spectracluster/similarity/MultiSimillarityTest.java
+++ trunk/src/test/java/uk/ac/ebi/pride/spectracluster/similarity/MultiSimillarityTest.java
@@ -16,11 +16,12 @@ import java.util.*;
 public class MultiSimillarityTest {
     private ConsensusSpectraItems[] consensusSpectraItems;
 
-    public static final double SIMILAR_THRESHOLD = 10000; // this is really big
+    public static final double SIMILAR_THRESHOLD = 1.5; // this is really big
 
     @Before
     public void setUp() throws Exception {
         // load a file contains a list of clusters
+       // URL url = MultiSimillarityTest.class.getClassLoader().getResource ("uk/ac/ebi/pride/spectracluster/util/spectra_400.0_4.0.cgf");
         URL url = MultiSimillarityTest.class.getClassLoader().getResource("uk/ac/ebi/pride/spectracluster/util/spectra_400.0_4.0.cgf");
         if (url == null) {
             throw new IllegalStateException("no file for input found!");
@@ -41,23 +42,38 @@ public class MultiSimillarityTest {
             List<ISpectrum> spectra = cluster.getSpectra();
             if (spectra.size() <= 1)
                 continue;
-            // attila  do work!!!
+
 
             SimilarityChecker oldSimilarity = new FrankEtAlDotProductOld();
             SimilarityChecker newSimilarity = new FrankEtAlDotProduct();
 
-            double oldDP;
-            oldDP = oldSimilarity.assessSimilarity(spectra.get(0), spectra.get(1));
+/*            double oldDP;
+            oldDP = oldSimilarity.assessSimilarity(spectra.get(2), spectra.get(3));
             double newDP;
-            newDP = newSimilarity.assessSimilarity(spectra.get(0), spectra.get(1));
+            newDP = newSimilarity.assessSimilarity(spectra.get(2), spectra.get(3));*/
 
             //Assert.assertEquals(oldDP, newDP, 0.001 * (oldDP + newDP));
-            Assert.assertTrue(Math.abs(oldDP - newDP) < SIMILAR_THRESHOLD);
+            //Assert.assertTrue(Math.abs(oldDP - newDP) < SIMILAR_THRESHOLD);
+
+            for (int index1 =  0; index1 < spectra.size(); index1++ ) {
+                for (int index2 = index1 + 1; index2 < spectra.size(); index2++) {
+
+                    double oldDotP;
+                    oldDotP = oldSimilarity.assessSimilarity(spectra.get(index1), spectra.get(index2));
+                    double newDotP;
+                    newDotP = newSimilarity.assessSimilarity(spectra.get(index1), spectra.get(index2));
+
+                    Assert.assertEquals(oldDotP, newDotP, 1.5 * (oldDotP + newDotP));
+                    //Assert.assertTrue(Math.abs(oldDotP - newDotP) < SIMILAR_THRESHOLD);
+
+                }
+
+            }
 
 
 
-/*
-            for (ISpectrum iSpectrum : spectra) {
+
+/*            for (ISpectrum iSpectrum : spectra) {
 
 
                 //for (int i = 0; spectra.size() > i; i++) {
@@ -68,16 +84,16 @@ public class MultiSimillarityTest {
                 double newDP2;
                 newDP2 = newSimilarity.assessSimilarity(iSpectrum, consensusSpectrum);
 
-                double oldDP;
-                oldDP = oldSimilarity.assessSimilarity(iSpectrum, iSpectrum);
-                double newDP;
-                newDP = newSimilarity.assessSimilarity(iSpectrum, iSpectrum);
+                double oldDP3;
+                oldDP3 = oldSimilarity.assessSimilarity(iSpectrum, iSpectrum);
+                double newDP4;
+                newDP4 = newSimilarity.assessSimilarity(iSpectrum, iSpectrum);
 
                 //Assert.assertEquals(oldDP, newDP, 0.001 * (oldDP + newDP));
-                Assert.assertTrue(Math.abs(oldDP - newDP) < SIMILAR_THRESHOLD);
+                Assert.assertTrue(Math.abs(oldDP1 - newDP2) < SIMILAR_THRESHOLD);
 
 
-                // comparing consensusSp agains member spectra gives -Infinity intensity in the example consensus spectrum
+                // comparing consensusSp agains member spectra gives -Infinity intensity in the example testCluster.cgf consensus spectrum
 
             }*/
 
