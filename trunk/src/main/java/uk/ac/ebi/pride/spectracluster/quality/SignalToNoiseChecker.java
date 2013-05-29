35107639622a86587eee2331c91cd0f98712b6eb
lordjoe2000@gmail.com <lordjoe2000@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2013-05-29 19:50:13 +0000

    Quality works

--- trunk/src/main/java/uk/ac/ebi/pride/spectracluster/quality/SignalToNoiseChecker.java
+++ trunk/src/main/java/uk/ac/ebi/pride/spectracluster/quality/SignalToNoiseChecker.java
@@ -13,41 +13,51 @@ import java.util.*;
 public class SignalToNoiseChecker implements QualityScorer {
 
     public static final int NUMBER_HIGH_PEAKS = 6;
+
     /**
-   	 * Calculates a spectrum's signal-to-noise ratio
-   	 * by taking the 2nd-6th highest peak's intensity
-   	 * and dividing it by the median intensity of all
-   	 * peaks.
-   	 * This method should only be used on normalized
-   	 * spectra.
-   	 */
+     * Calculates a spectrum's signal-to-noise ratio
+     * by taking the 2nd-6th highest peak's intensity
+     * and dividing it by the median intensity of all
+     * peaks.
+     * This method should only be used on normalized
+     * spectra.
+     */
     @Override
     public double calculateQualityScore(ISpectrum spectrum) {
         ISpectrum highestNPeaks = spectrum.getHighestNPeaks(NUMBER_HIGH_PEAKS);
 
-         // TODO: react to small spectra
-		if (highestNPeaks.getPeaksCount() < NUMBER_HIGH_PEAKS)
-			return 0.0;
-        double meanHigh = highestNPeaks.getTotalIntensity() / NUMBER_HIGH_PEAKS;
+        // TODO: react to small spectra
+        if (highestNPeaks.getPeaksCount() < NUMBER_HIGH_PEAKS)
+            return 0.0;
+        double totalIntensity = highestNPeaks.getTotalIntensity();
+        double highestPeak = 0;
+        for (IPeak peak : highestNPeaks.getPeaks()) {
+            highestPeak = Math.max(peak.getIntensity(), highestPeak);
+        }
+
+        double meanHigh = (totalIntensity - highestPeak) / (NUMBER_HIGH_PEAKS - 1);
 
         List<IPeak> peaks = spectrum.getPeaks();
-        Collections.sort(peaks,PeakIntensityComparator.INSTANCE);
+        Collections.sort(peaks, PeakIntensityComparator.INSTANCE);
 
         double median;
 
-        if(peaks.size() % 2 == 1)  {
-            IPeak iPeak = peaks.get((peaks.size() / 2) + 1);
+        int peakSize = peaks.size();
+        if (peakSize % 2 == 1) {
+            int index = peakSize / 2;
+            IPeak iPeak = peaks.get(index);
             median = iPeak.getIntensity(); // odd case
 
-        }
-        else {
-            IPeak iPeak1 = peaks.get((peaks.size() / 2));
-            IPeak iPeak2 = peaks.get((peaks.size() / 2) + 1);
+        } else {
+            int index2 = (peakSize / 2);
+            int index1 = index2 - 1;
+            IPeak iPeak1 = peaks.get(index1);
+            IPeak iPeak2 = peaks.get(index2);
             double intensity1 = iPeak1.getIntensity();
             double intensity2 = iPeak2.getIntensity();
-             median = (intensity1 + intensity2) / 2; // even case
-         }
+            median = (intensity1 + intensity2) / 2; // even case
+        }
 
         return meanHigh / median;
-     }
+    }
 }
