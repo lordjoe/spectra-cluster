44807ddbec832682169fe3c0032a73db5c933d11
harry.r.wang@gmail.com <harry.r.wang@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2013-07-22 20:38:10 +0000

    add merge identical peak tests

---
+++ trunk/src/test/java/uk/ac/ebi/pride/spectracluster/consensus/MergeIdenticalPeaksComparisonTests.java
@@ -0,0 +1,80 @@
+package uk.ac.ebi.pride.spectracluster.consensus;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.ebi.pride.spectracluster.spectrum.IPeak;
+import uk.ac.ebi.pride.spectracluster.spectrum.IPeptideSpectrumMatch;
+import uk.ac.ebi.pride.spectracluster.spectrum.ISpectrum;
+import uk.ac.ebi.pride.tools.fast_spectra_clustering.ClusteringTestUtilities;
+import uk.ac.ebi.pride.tools.jmzreader.model.Spectrum;
+import uk.ac.ebi.pride.tools.pride_spectra_clustering.impl.Adapters;
+import uk.ac.ebi.pride.tools.pride_spectra_clustering.util.Peak;
+
+import java.util.*;
+
+/**
+ * @author Rui Wang
+ * @version $Id$
+ */
+public class MergeIdenticalPeaksComparisonTests {
+
+    private FrankEtAlConsensusSpectrumBuilder frankEtAlConsensusSpectrumBuilder;
+    private uk.ac.ebi.pride.tools.pride_spectra_clustering.consensus_spectrum_builder.impl.FrankEtAlConsensusSpectrumBuilder originalFrankEtAlConsensusSpectrumBuilder;
+
+    @Before
+    public void setUp() throws Exception {
+        frankEtAlConsensusSpectrumBuilder = new FrankEtAlConsensusSpectrumBuilder();
+        originalFrankEtAlConsensusSpectrumBuilder = new uk.ac.ebi.pride.tools.pride_spectra_clustering.consensus_spectrum_builder.impl.FrankEtAlConsensusSpectrumBuilder();
+
+    }
+
+    @Test
+    public void testSpectrumBuilderComparison() throws Exception {
+        List<IPeptideSpectrumMatch> spectra = ClusteringTestUtilities.readISpectraFromResource();
+        List<Spectrum> originalSpectra = ClusteringTestUtilities.readSpectrumsFromResource();
+
+        Assert.assertEquals(spectra.size(), originalSpectra.size());
+
+        for (int i = 0; i < spectra.size() - 1; i++) {
+            ISpectrum spectrum = spectra.get(i);
+            ISpectrum nextSpectrum = spectra.get(i + 1);
+
+            List<IPeak> peaks = frankEtAlConsensusSpectrumBuilder.addAllPeaks(Arrays.asList(spectrum, nextSpectrum));
+            List<Peak> originalPeaks = Adapters.fromIPeaks(peaks);
+            Map<Double, Peak> originalPeakMap = createPeakMap(originalPeaks);
+            List<IPeak> mergedPeaks = frankEtAlConsensusSpectrumBuilder.mergeIdenticalPeaks(peaks);
+            List<Peak> originalMergedPeaks = originalFrankEtAlConsensusSpectrumBuilder.mergeIdenticalPeaks(originalPeakMap);
+            List<IPeak> convertedOriginalMergedPeaks = Adapters.fromPeaks(originalMergedPeaks);
+
+            Collections.sort(mergedPeaks);
+            Collections.sort(convertedOriginalMergedPeaks);
+
+            Assert.assertEquals(mergedPeaks.size(), convertedOriginalMergedPeaks.size());
+
+            for (int j = 0; j < mergedPeaks.size(); j++) {
+                IPeak mergedPeak = mergedPeaks.get(j);
+                IPeak originalMergedPeak = convertedOriginalMergedPeaks.get(j);
+                boolean equivalent = mergedPeak.equivalent(originalMergedPeak);
+                if (!equivalent) {
+                    Assert.assertTrue(equivalent);
+                }
+            }
+
+        }
+
+    }
+
+
+    private Map<Double, Peak> createPeakMap(List<Peak> originalPeaks) {
+        Map<Double, Peak> originalPeakMap = new LinkedHashMap<Double, Peak>();
+
+        for (Peak originalPeak : originalPeaks) {
+            originalPeakMap.put(originalPeak.getMz(), originalPeak);
+        }
+
+
+        return originalPeakMap;
+    }
+
+}
