3715bb98c7915d86bec63cb31af18f9528957363
harry.r.wang@gmail.com <harry.r.wang@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2013-11-12 11:17:48 +0000

    add mgf cluster db loading job

--- spectra-cluster/branches/development/src/main/java/uk/ac/ebi/pride/spectracluster/clustersmilarity/ClusterSimilarityUtilities.java
+++ spectra-cluster/branches/development/src/main/java/uk/ac/ebi/pride/spectracluster/clustersmilarity/ClusterSimilarityUtilities.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.pride.spectracluster.clustersmilarity;
 
 import uk.ac.ebi.pride.spectracluster.cluster.ISpectralCluster;
 import uk.ac.ebi.pride.spectracluster.spectrum.IPeptideSpectrumMatch;
+import uk.ac.ebi.pride.spectracluster.spectrum.PeptideSpectrumMatch;
 import uk.ac.ebi.pride.spectracluster.util.ParserUtilities;
 
 import java.io.File;
@@ -10,6 +11,7 @@ import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -28,7 +30,7 @@ public class ClusterSimilarityUtilities {
                 IClusterSet clusterSet = buildFromClusteringFile(file1, spectrumRetriever);
                 simpleClusterSet.addClusters(clusterSet.getClusters());
             }
-        } else if (file.getName().endsWith(".clustering")){
+        } else if (file.getName().endsWith(".clustering")) {
             try {
                 LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(file));
                 ISpectralCluster[] clusters = ParserUtilities.readClustersFromClusteringFile(lineNumberReader, spectrumRetriever);
@@ -47,47 +49,79 @@ public class ClusterSimilarityUtilities {
             for (File file1 : files) {
                 buildFromMgfFile(file1, spectrumRetriever);
             }
-        } else if (file.getName().endsWith(".mgf")){
+        } else if (file.getName().endsWith(".mgf")) {
             try {
+                System.out.println(file.getName());
                 LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(file));
                 IPeptideSpectrumMatch[] spectra = ParserUtilities.readMGFScans(lineNumberReader);
-                for (IPeptideSpectrumMatch readSpectrum : spectra) {
-
-                    spectrumRetriever.addSpectrum(spectra);
-                }
+                spectrumRetriever.addSpectra(spectra);
+                lineNumberReader.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
     }
 
+    public static void buildFromTSVFile(File file, IMutableSpectrumRetriever spectrumRetriever) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File file1 : files) {
+                buildFromTSVFile(file1, spectrumRetriever);
+            }
+        } else if (file.getName().endsWith(".tsv")) {
+            try {
+                LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(file));
+                String line;
+                while((line = lineNumberReader.readLine()) != null) {
+                    IPeptideSpectrumMatch peptideSpectrumMatch = constructPeptideSpectrumMatch(line);
+                    spectrumRetriever.addSpectra(peptideSpectrumMatch);
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static IPeptideSpectrumMatch constructPeptideSpectrumMatch(String line) {
+        String[] parts = line.split("\t");
+        String spectrumId = parts[0];
+        int precursorCharge = Integer.parseInt(parts[1]);
+        float precursorMz = Float.parseFloat(parts[2]);
+        String peptide = parts[3];
+
+        return new PeptideSpectrumMatch(spectrumId, peptide, precursorCharge, precursorMz, Collections.EMPTY_LIST);
+    }
+
     /**
      * return all clusters with more than one spectrum
+     *
      * @param clusters !null list of clusters
-     * @return  !null list of clusters
+     * @return !null list of clusters
      */
-    public static List<ISpectralCluster>  nonSingleClusters( List<ISpectralCluster> clusters)     {
+    public static List<ISpectralCluster> nonSingleClusters(List<ISpectralCluster> clusters) {
         List<ISpectralCluster> holder = new ArrayList<ISpectralCluster>();
-          for (ISpectralCluster cluster : clusters) {
-            if(cluster.getClusteredSpectraCount() > 1)
+        for (ISpectralCluster cluster : clusters) {
+            if (cluster.getClusteredSpectraCount() > 1)
                 holder.add(cluster);
         }
         return holder;
     }
 
     /**
-      * return all clusters with only  one spectrum
-      * @param clusters !null list of clusters
-      * @return  !null list of clusters
-      */
-     public static List<ISpectralCluster>  singleClusters( List<ISpectralCluster> clusters)     {
-         List<ISpectralCluster> holder = new ArrayList<ISpectralCluster>();
-           for (ISpectralCluster cluster : clusters) {
-             if(cluster.getClusteredSpectraCount() == 1)
-                 holder.add(cluster);
-         }
-         return holder;
-     }
+     * return all clusters with only  one spectrum
+     *
+     * @param clusters !null list of clusters
+     * @return !null list of clusters
+     */
+    public static List<ISpectralCluster> singleClusters(List<ISpectralCluster> clusters) {
+        List<ISpectralCluster> holder = new ArrayList<ISpectralCluster>();
+        for (ISpectralCluster cluster : clusters) {
+            if (cluster.getClusteredSpectraCount() == 1)
+                holder.add(cluster);
+        }
+        return holder;
+    }
 
 
     public static void main(String[] args) {
