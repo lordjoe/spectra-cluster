3ba3f222b8387fbc7e9bf7e2f302e23bbe8f8af6
Lordjoe2000@gmail.com <Lordjoe2000@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2014-02-20 01:23:45 +0000

    Modified Closest Peaks test and added code to append compared peaks in our dot product when an appendable in Defaults is set - Please use for debugging only

--- spectra-cluster-hadoop/branches/development/src/main/java/uk/ac/ebi/pride/spectracluster/hadoop/ClusterLauncherJobBuilder.java
+++ spectra-cluster-hadoop/branches/development/src/main/java/uk/ac/ebi/pride/spectracluster/hadoop/ClusterLauncherJobBuilder.java
@@ -2,7 +2,6 @@ package uk.ac.ebi.pride.spectracluster.hadoop;
 
 import org.systemsbiology.hadoop.*;
 
-import java.io.*;
 import java.util.*;
 
 /**
@@ -15,23 +14,51 @@ public class ClusterLauncherJobBuilder extends AbstractClusterLauncherJobBuilder
 
     public static final IJobBuilderFactory FACTORY = new IJobBuilderFactory() {
         @Override
-        public IJobBuilder getJobBuilder(IStreamOpener launcher,Object... otherData) {
-            return new  ClusterLauncherJobBuilder((ClusterLauncher)launcher);
+        public IJobBuilder getJobBuilder(IStreamOpener launcher, Object... otherData) {
+            return new ClusterLauncherJobBuilder((ClusterLauncher) launcher);
         }
     };
 
-    public static final Class[] JOB_CLASSES =
+    public static final Class[] DEFAULT_JOB_CLASSES =
             {
                     SpectraPeakClustererPass1.class,
                     SpectraClustererMergerOffset.class,
                     SpectraClustererMerger.class,
-                     ClusterConsolidator.class
-            } ;
+                    ClusterConsolidator.class
+            };
 
 
+    protected static Class[] makeJobClasses(ClusterLauncher launcher) {
+        IParameterHolder application = launcher.getApplication();
+        String steps = application.getParameter("uk.ac.ebi.pride.spectracluster.hadoop.ClusterLauncher.Steps");
+        if (steps != null)
+            return buildStepClasses(steps);
+        else
+            return DEFAULT_JOB_CLASSES;
+
+    }
+
+    protected static Class[] buildStepClasses(final String pSteps) {
+        try {
+            String[] classNames = pSteps.split(",");
+            List<Class> holder = new ArrayList<Class>();
+
+            for (int i = 0; i < classNames.length; i++) {
+                String className = classNames[i].trim();
+                holder.add(Class.forName(className));
+            }
+            Class[] ret = new Class[holder.size()];
+            holder.toArray(ret);
+            return ret;
+        }
+        catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
 
     public ClusterLauncherJobBuilder(ClusterLauncher launcher) {
-        super(launcher,JOB_CLASSES);
+        super(launcher, makeJobClasses(launcher));
     }
 
     /**
@@ -42,7 +69,7 @@ public class ClusterLauncherJobBuilder extends AbstractClusterLauncherJobBuilder
     @Override
     public int getNumberJobs() {
         // patch for development
-        if(true)
+        if (true)
             return 4;
         return super.getNumberJobs();
 
--- spectra-cluster/branches/development/src/test/java/uk/ac/ebi/pride/spectracluster/similarity/ClosestPeaksTest.java
+++ spectra-cluster/branches/development/src/test/java/uk/ac/ebi/pride/spectracluster/similarity/ClosestPeaksTest.java
@@ -1,12 +1,13 @@
 package uk.ac.ebi.pride.spectracluster.similarity;
 
-import junit.framework.TestCase;
-import org.junit.Before;
+import junit.framework.*;
+import org.eclipse.jdt.internal.core.*;
 import org.junit.Test;
-import uk.ac.ebi.pride.spectracluster.spectrum.IPeptideSpectrumMatch;
-import uk.ac.ebi.pride.spectracluster.util.ClusteringTestUtilities;
+import uk.ac.ebi.pride.spectracluster.spectrum.*;
+import uk.ac.ebi.pride.spectracluster.util.*;
 
-import java.util.List;
+import javax.xml.bind.annotation.adapters.*;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,40 +17,137 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class ClosestPeaksTest {
-    private FrankEtAlDotProductTester checker = new FrankEtAlDotProductTester();
-    List<IPeptideSpectrumMatch> spectra;
+
+    public static final String[] INTERESTING_IDS =
+            {
+                    "1248200",
+                    "1249768",
+                    "1249841",
+                    "1250984",
+//                    "1252317",
+//                    "1252580",
+//                    "1252923",
+//                    "18834",
+//                    "58604",
+//                    "99609",
+            };
+
+    public static Set<String>  INTERESTING_ID_SET = new HashSet<String>(Arrays.asList(INTERESTING_IDS));
+
+    /**
+     * really here to print out the peaks compared on a few interesting cases where the results are different
+     */
+    @Test
+     public void showHandledPeaksForInterestingCases() {
+         List<IPeptideSpectrumMatch> spectra = ClusteringTestUtilities.readISpectraFromResource();
+
+         IPeptideSpectrumMatch[] spectrums = (IPeptideSpectrumMatch[]) spectra.toArray();
+
+          FrankEtAlDotProductTester checker = new FrankEtAlDotProductTester();
+         FrankEtAlDotProduct currentChecker = new FrankEtAlDotProduct();
+
+         Set<String> interestingIds = new HashSet<String>() ;
 
 
-    @Before
-    public void setUp() throws Exception {
-        spectra = ClusteringTestUtilities.readISpectraFromResource();
-    }
+         for (int i = 0; i < spectrums.length; i++) {
+             IPeptideSpectrumMatch psm1 = spectrums[i];
+             String id1 = psm1.getId();
+             if(!INTERESTING_ID_SET.contains(id1))
+                 continue; // not an interesting case
+
+             for (int j = i + 1; j < spectrums.length; j++) {
+                 IPeptideSpectrumMatch psm2 = spectrums[j];
+
+                 String id2 = psm2.getId();
+                 if(!INTERESTING_ID_SET.contains(id2))
+                  continue; // not an interesting case
+
+                 System.out.println("Comparing " + id1 + " " + id2);
+
+                 StringBuilder usedPeaksTester = new StringBuilder();
+                 Defaults.INSTANCE.setDebugOutput(usedPeaksTester);
+
+
+                 checker.setUseClosestPeak(true);
+                 double dotOrg = checker.assessSimilarity(psm1, psm2);
+
+                 System.out.println("Peaks compared original Frank Et Al (when the code is written)");
+                 // print usage
+                 System.out.println(usedPeaksTester.toString());
+
+                 usedPeaksTester.setLength(0);  // clear debug output
+                 double dotNew = currentChecker.assessSimilarity(psm1, psm2);
+
+                     // print usage
+                 System.out.println("Peaks compared current Frank Et Al ");
+                 System.out.println(usedPeaksTester.toString());
+
+
+             }
+
+         }
+      }
 
     @Test
     public void testDifferentDotProducts() {
+        List<IPeptideSpectrumMatch> spectra = ClusteringTestUtilities.readISpectraFromResource();
+
+        IPeptideSpectrumMatch[] spectrums = (IPeptideSpectrumMatch[]) spectra.toArray();
+
         int total = 0;
         int different = 0;
+        FrankEtAlDotProductTester checker = new FrankEtAlDotProductTester();
+        FrankEtAlDotProduct currentChecker = new FrankEtAlDotProduct();
 
-        for (IPeptideSpectrumMatch psm1 : spectra) {
-            for (IPeptideSpectrumMatch psm2 : spectra) {
+        Set<String> interestingIds = new HashSet<String>() ;
+
+
+        for (int i = 0; i < spectrums.length; i++) {
+            IPeptideSpectrumMatch psm1 = spectrums[i];
+            for (int j = i + 1; j < spectrums.length; j++) {
+                IPeptideSpectrumMatch psm2 = spectrums[j];
                 checker.setUseClosestPeak(true);
                 double dotOrg = checker.assessSimilarity(psm1, psm2);
+
+                // ignore poor matches
+             //   if (dotOrg < 0.5)
+              //      continue;
 
                 checker.setUseClosestPeak(false);
                 double dotNew = checker.assessSimilarity(psm1, psm2);
 
-                if (dotOrg -0.01 > dotNew || dotOrg + 0.01 < dotNew) {
+                if (dotOrg - 0.01 > dotNew || dotOrg + 0.01 < dotNew) {
                     different++;
+                    if (dotOrg >= 0.7) {
+                        // these are the really interesting cases
+                        checker.setUseClosestPeak(true);
+                        dotOrg = checker.assessSimilarity(psm1, psm2);
 
-                    if (dotOrg >= 0.7)
-                        System.out.printf("Old: %f\tNew: %f\tDiff: %f\n", dotOrg, dotNew, dotOrg - dotNew);
+                        double noClosestPeak = dotNew;
+                        dotNew = currentChecker.assessSimilarity(psm1, psm2);
+                        String id2 = psm2.getId();
+                        String id1 = psm1.getId();
+                        interestingIds.add(id1);
+                        interestingIds.add(id2);
+
+                        System.out.printf(id2 + ":" + id1 + " " +  "Old: %8.3f Newx: %8.3f New: %8.3f\tDiff: %8.3f\n", dotOrg,noClosestPeak, dotNew, dotOrg - dotNew);
+                    }
                 }
-
-
-
                 total++;
+
             }
+
         }
+
+        List<String>  sorted = new ArrayList<String>(interestingIds);
+        Collections.sort(sorted);
+        System.out.println("Interesting Ids");
+        for (String s : sorted) {
+            System.out.println(s);
+        }
+
+
+
 
         TestCase.assertEquals(total, different, 0.2);
     }
