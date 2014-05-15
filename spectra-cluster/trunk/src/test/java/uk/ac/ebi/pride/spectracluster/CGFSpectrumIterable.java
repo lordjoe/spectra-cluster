f03a0e448c3638a79f10aad1258237616d82c933
harry.r.wang@gmail.com <harry.r.wang@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2014-05-15 14:50:39 +0000

    Moved CGFSpectrumIterable to test

--- spectra-cluster/trunk/src/test/java/uk/ac/ebi/pride/spectracluster/CGFSpectrumIterable.java
+++ spectra-cluster/trunk/src/test/java/uk/ac/ebi/pride/spectracluster/CGFSpectrumIterable.java
@@ -1,4 +1,4 @@
-package uk.ac.ebi.pride.spectracluster.spectrum;
+package uk.ac.ebi.pride.spectracluster;
 
 import uk.ac.ebi.pride.spectracluster.cluster.ISpectralCluster;
 import uk.ac.ebi.pride.spectracluster.util.ParserUtilities;
