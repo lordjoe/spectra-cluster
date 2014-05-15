d0ba2cb2e60aee15368ecddf749e043ce590881a
harry.r.wang <harry.r.wang@gmail.com>
2014-05-15 13:23:29 +0000

    remove getPeakCount() and getPeaks() from SpectralCluster

---
+++ spectra-cluster-hadoop/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/hadoop/MajorPeakReducer.java
@@ -0,0 +1,126 @@
+package uk.ac.ebi.pride.spectracluster.hadoop;
+
+import com.lordjoe.algorithms.IWideBinner;
+import org.apache.hadoop.io.Text;
+import uk.ac.ebi.pride.spectracluster.cluster.IIncrementalClusteringEngine;
+import uk.ac.ebi.pride.spectracluster.cluster.ISpectralCluster;
+import uk.ac.ebi.pride.spectracluster.keys.ChargeMZKey;
+import uk.ac.ebi.pride.spectracluster.keys.ChargePeakMZKey;
+import uk.ac.ebi.pride.spectracluster.spectrum.IPeptideSpectrumMatch;
+import uk.ac.ebi.pride.spectracluster.util.Defaults;
+import uk.ac.ebi.pride.spectracluster.util.ParserUtilities;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.StringReader;
+import java.util.Collection;
+
+/**
+ * uk.ac.ebi.pride.spectracluster.hadoop.MajorPeakReducer
+ * Form clusters from peaks
+ */
+public class MajorPeakReducer extends AbstractClusteringEngineReducer {
+
+
+
+    private double majorPeakWindowSize = Defaults.getMajorPeakMZWindowSize();
+
+
+    public double getMajorPeakWindowSize() {
+        return majorPeakWindowSize;
+    }
+
+
+
+    @Override
+    public void reduceNormal(Text key, Iterable<Text> values,
+                             Context context) throws IOException, InterruptedException {
+
+        String keyStr = key.toString();
+        ChargePeakMZKey mzKey = new ChargePeakMZKey(keyStr);
+
+        // if we are in a different bin - different charge or peak
+        if (mzKey.getCharge() != getCurrentCharge() || mzKey.getPeakMZ() != getMajorPeak()) {
+            updateEngine(context, mzKey);
+        }
+
+        IIncrementalClusteringEngine engine = getEngine();
+        if (engine == null)
+            return; // very occasionally  we get null - not sure why
+
+        //noinspection LoopStatementThatDoesntLoop
+        for (Text val : values) {
+            String valStr = val.toString();
+
+
+            LineNumberReader rdr = new LineNumberReader((new StringReader(valStr)));
+            final IPeptideSpectrumMatch match = ParserUtilities.readMGFScan(rdr);
+            if (match == null)
+                continue; // not sure why this happens but nothing seems like the thing to do
+            final ISpectralCluster cluster = match.asCluster();
+
+
+            final Collection<ISpectralCluster> removedClusters = engine.addClusterIncremental(cluster);
+
+            writeClusters(context, removedClusters);
+
+
+        }
+    }
+
+    /**
+     * this version of writeCluster does all the real work
+     * @param context
+     * @param cluster
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    protected void writeOneVettedCluster(@Nonnull final Context context,@Nonnull  final ISpectralCluster cluster) throws IOException, InterruptedException {
+        ChargeMZKey key = new ChargeMZKey(cluster.getPrecursorCharge(), cluster.getPrecursorMz());
+
+        StringBuilder sb = new StringBuilder();
+        cluster.append(sb);
+        String string = sb.toString();
+
+        if (string.length() > SpectraHadoopUtilities.MIMIMUM_CLUSTER_LENGTH) {
+            writeKeyValue(key.toString(), string, context);
+            incrementBinCounters(key, context); // how big are the bins - used in next job
+        }
+    }
+
+    protected void incrementBinCounters(ChargeMZKey mzKey, Context context) {
+         IWideBinner binner = Defaults.DEFAULT_WIDE_MZ_BINNER;
+         int[] bins = binner.asBins(mzKey.getPrecursorMZ());
+         //noinspection ForLoopReplaceableByForEach
+         for (int i = 0; i < bins.length; i++) {
+             int bin = bins[i];
+             SpectraHadoopUtilities.incrementPartitionCounter(context, "Bin", bin);
+
+         }
+
+     }
+
+
+
+    protected <T> boolean updateEngine(final Context context, final T key) throws IOException, InterruptedException
+      {
+          ChargePeakMZKey pMzKey = (ChargePeakMZKey)key;
+
+          if (getEngine() != null) {
+              final Collection<ISpectralCluster> clusters = getEngine().getClusters();
+              writeClusters(context, clusters);
+              setEngine(null);
+          }
+          // if not at end make a new engine
+          if (pMzKey != null) {
+              setEngine(getFactory().getIncrementalClusteringEngine( getMajorPeakWindowSize()));
+              setMajorPeak(pMzKey.getPeakMZ());
+              setCurrentCharge(pMzKey.getCharge());
+          }
+         return true;
+     }
+
+
+
+}
---
+++ spectra-cluster-hadoop/trunk/src/test/java/uk/ac/ebi/pride/spectracluster/hadoop/KeyTests.java
@@ -0,0 +1,110 @@
+package uk.ac.ebi.pride.spectracluster.hadoop;
+
+import com.lordjoe.algorithms.*;
+import org.junit.*;
+import uk.ac.ebi.pride.spectracluster.keys.*;
+import uk.ac.ebi.pride.spectracluster.spectrum.*;
+import uk.ac.ebi.pride.spectracluster.util.*;
+
+import java.util.*;
+
+/**
+ * uk.ac.ebi.pride.spectracluster.hadoop.KeyTests
+ * User: Steve
+ * Date: 8/13/13
+ */
+public class KeyTests {
+
+    @Test
+    public void testKeys() throws Exception {
+        List<ChargePeakMZKey> keyholder = new ArrayList<ChargePeakMZKey>();
+        final List<? extends ISpectrum> specs = ClusteringDataUtilities.readISpectraFromResource();
+        for (ISpectrum spec : specs) {
+            keyholder.addAll(validateSpectrum(spec));
+        }
+        validatePartitioner(keyholder)  ;
+    }
+
+    public static final int NUMBER_BUCKETS = 20;
+    protected void validatePartitioner(List<ChargePeakMZKey> keyholder) {
+        int[] counts = new int[NUMBER_BUCKETS];
+        for (ChargePeakMZKey key : keyholder) {
+             int hash = key.getPartitionHash();
+             counts[hash % NUMBER_BUCKETS]++;
+        }
+        int maxCounts = 0;
+        int minCounts = Integer.MAX_VALUE;
+        //noinspection ForLoopReplaceableByForEach
+       for (int i = 0; i < counts.length; i++) {
+            int count = counts[i];
+           maxCounts = Math.max(maxCounts,count);
+           minCounts = Math.min(minCounts, count);
+
+        }
+        Assert.assertTrue( 1.3 * minCounts > maxCounts); // 30% of each other
+    }
+
+    protected List<ChargePeakMZKey> validateSpectrum(ISpectrum spec) {
+        final double precursorMz = spec.getPrecursorMz();
+        final int charge = spec.getPrecursorCharge();
+
+        MZKey mzKey1 = new MZKey(precursorMz);
+        ChargeMZKey k2 = new ChargeMZKey(charge, precursorMz);
+        // better not crash
+
+        String s1 = mzKey1.toString();
+        String s2 = k2.toString();
+        Assert.assertEquals(s2, String.format("%02d", charge) + ":" + s1);
+
+        MZKey mzKey2 = new MZKey(s1);
+        Assert.assertEquals(mzKey1,mzKey2);
+
+
+        ChargeMZKey m3 = new ChargeMZKey(s2);
+        Assert.assertEquals(k2,m3);
+
+        ChargePeakMZKey chargePeakKey1 = new ChargePeakMZKey(charge, precursorMz,precursorMz);
+        ChargePeakMZKey chargePeakKey2 = new ChargePeakMZKey(chargePeakKey1.toString());
+        if(!chargePeakKey1.equals(chargePeakKey2))
+          Assert.assertEquals(chargePeakKey1,chargePeakKey2);
+
+        IWideBinner binner = Defaults.DEFAULT_WIDE_MZ_BINNER;
+
+        List<String> holder = new ArrayList<String>();
+        List<ChargePeakMZKey> keyholder = new ArrayList<ChargePeakMZKey>();
+        @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+        List<ChargeBinMZKey> binKeyholder = new ArrayList<ChargeBinMZKey>();
+        for (IPeak pk : spec.getPeaks()) {
+            final float mz = pk.getMz();
+            ChargePeakMZKey key = new ChargePeakMZKey(charge, mz, precursorMz);
+            int[] bins = binner.asBins(precursorMz);
+            //noinspection ForLoopReplaceableByForEach
+            for (int i = 0; i < bins.length; i++) {
+                int bin = bins[i];
+                ChargeBinMZKey binkey = new ChargeBinMZKey(charge, bin, precursorMz);
+                binKeyholder.add(binkey) ;
+                String binKeyStr = binkey.toString();
+                ChargeBinMZKey binky2 = new ChargeBinMZKey(binKeyStr);
+                 Assert.assertEquals(binkey,binky2);
+            }
+            String keyStr = key.toString();
+            ChargePeakMZKey ky2 = new ChargePeakMZKey(keyStr);
+            Assert.assertEquals(key,ky2);
+            holder.add(keyStr);
+            keyholder.add(key);
+        }
+        // make a sorted collection
+        List<String> holderSort = new ArrayList<String>(holder);
+        Collections.sort(holderSort);
+        // because peaks ar MZ sorted keys should be
+        Assert.assertArrayEquals(holderSort.toArray(),holder.toArray());
+
+        // make a sorted collection
+        List<ChargePeakMZKey> keyholderSort = new ArrayList<ChargePeakMZKey>(keyholder);
+        Collections.sort(keyholderSort);
+       // because peaks ar MZ sorted keys should be
+        Assert.assertArrayEquals(keyholderSort.toArray(),keyholder.toArray());
+
+        return keyholder;
+    }
+}
