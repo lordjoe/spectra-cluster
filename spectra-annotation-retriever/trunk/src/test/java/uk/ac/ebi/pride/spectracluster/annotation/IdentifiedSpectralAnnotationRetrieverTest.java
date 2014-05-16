4905383f95847c25458bb151a4398772e08d27d7
harry.r.wang <harry.r.wang@gmail.com>
2014-05-16 14:41:00 +0000

    unit tests added

---
+++ spectra-annotation-retriever/trunk/src/test/java/uk/ac/ebi/pride/spectracluster/annotation/IdentifiedSpectralAnnotationRetrieverTest.java
@@ -0,0 +1,21 @@
+package uk.ac.ebi.pride.spectracluster.annotation;
+
+import org.junit.Test;
+
+public class IdentifiedSpectralAnnotationRetrieverTest {
+
+    @Test
+    public void testGetAnnotation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAnnotations() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAnnotations1() throws Exception {
+
+    }
+}
