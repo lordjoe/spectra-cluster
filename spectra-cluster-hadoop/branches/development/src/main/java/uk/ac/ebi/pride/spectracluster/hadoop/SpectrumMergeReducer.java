689ba77e950ccb83fd863042357f1e7f9b504937
Lordjoe2000 <lordjoe2000@gmail.com>
2014-05-19 16:23:25 +0000

    added spectrum filter

--- spectra-cluster-hadoop/branches/development/src/main/java/uk/ac/ebi/pride/spectracluster/hadoop/SpectrumMergeReducer.java
+++ spectra-cluster-hadoop/branches/development/src/main/java/uk/ac/ebi/pride/spectracluster/hadoop/SpectrumMergeReducer.java
@@ -4,8 +4,6 @@ import com.lordjoe.algorithms.*;
 import org.apache.hadoop.io.*;
 import uk.ac.ebi.pride.spectracluster.cluster.*;
 import uk.ac.ebi.pride.spectracluster.clustersmilarity.*;
-import uk.ac.ebi.pride.spectracluster.engine.IIncrementalClusteringEngine;
-import uk.ac.ebi.pride.spectracluster.engine.IncrementalClusteringEngine;
 import uk.ac.ebi.pride.spectracluster.keys.*;
 import uk.ac.ebi.pride.spectracluster.spectrum.*;
 import uk.ac.ebi.pride.spectracluster.util.*;
