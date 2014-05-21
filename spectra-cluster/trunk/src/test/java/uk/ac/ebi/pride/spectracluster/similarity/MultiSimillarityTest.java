53ece1ab9cef572f3ff8d93c4d141430ddeafe03
harry.r.wang <harry.r.wang@gmail.com>
2014-05-21 13:50:11 +0000

    Remove debug variable from FrankEtAlDotProduct

--- spectra-cluster/trunk/src/test/java/uk/ac/ebi/pride/spectracluster/similarity/MultiSimillarityTest.java
+++ spectra-cluster/trunk/src/test/java/uk/ac/ebi/pride/spectracluster/similarity/MultiSimillarityTest.java
@@ -49,55 +49,15 @@ public class MultiSimillarityTest {
             ISimilarityChecker oldSimilarity = new FrankEtAlDotProductOld();
             ISimilarityChecker newSimilarity = new FrankEtAlDotProduct();
 
-/*            double oldDP;
-            oldDP = oldSimilarity.assessSimilarity(spectra.get(2), spectra.get(3));
-            double newDP;
-            newDP = newSimilarity.assessSimilarity(spectra.get(2), spectra.get(3));*/
-
-            //Assert.assertEquals(oldDP, newDP, 0.001 * (oldDP + newDP));
-            //Assert.assertTrue(Math.abs(oldDP - newDP) < SIMILAR_THRESHOLD);
-
             for (int index1 = 0; index1 < spectra.size(); index1++) {
                 for (int index2 = index1 + 1; index2 < spectra.size(); index2++) {
 
-                    double oldDotP;
-                    oldDotP = oldSimilarity.assessSimilarity(spectra.get(index1), spectra.get(index2));
-                    double newDotP;
-                    newDotP = newSimilarity.assessSimilarity(spectra.get(index1), spectra.get(index2));
+                    double oldDotP = oldSimilarity.assessSimilarity(spectra.get(index1), spectra.get(index2));
+                    double newDotP = newSimilarity.assessSimilarity(spectra.get(index1), spectra.get(index2));
 
                     Assert.assertEquals(oldDotP, newDotP, 0.00001);
-                    //Assert.assertTrue(Math.abs(oldDotP - newDotP) < SIMILAR_THRESHOLD);
-
                 }
-
             }
-
-
-
-
-/*            for (ISpectrum iSpectrum : spectra) {
-
-
-                //for (int i = 0; spectra.size() > i; i++) {
-                //   ISpectrum  spectrum = spectra[i];
-
-                double oldDP1;
-                oldDP1 = oldSimilarity.assessSimilarity(iSpectrum, consensusSpectrum);
-                double newDP2;
-                newDP2 = newSimilarity.assessSimilarity(iSpectrum, consensusSpectrum);
-
-                double oldDP3;
-                oldDP3 = oldSimilarity.assessSimilarity(iSpectrum, iSpectrum);
-                double newDP4;
-                newDP4 = newSimilarity.assessSimilarity(iSpectrum, iSpectrum);
-
-                //Assert.assertEquals(oldDP, newDP, 0.001 * (oldDP + newDP));
-                Assert.assertTrue(Math.abs(oldDP1 - newDP2) < SIMILAR_THRESHOLD);
-
-
-                // comparing consensusSp agains member spectra gives -Infinity intensity in the example testCluster.cgf consensus spectrum
-
-            }*/
 
         }
 
