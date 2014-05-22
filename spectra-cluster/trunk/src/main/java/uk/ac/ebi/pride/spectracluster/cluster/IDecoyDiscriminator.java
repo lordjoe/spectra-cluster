fb2743840eeb0e466cbae11a7da65eae6bdb8134
harry.r.wang@gmail.com <harry.r.wang@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2014-05-22 12:43:26 +0000

    Mark IDecoyDiscriminator as development

--- spectra-cluster/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/cluster/IDecoyDiscriminator.java
+++ spectra-cluster/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/cluster/IDecoyDiscriminator.java
@@ -4,7 +4,10 @@ package uk.ac.ebi.pride.spectracluster.cluster;
  * uk.ac.ebi.pride.spectracluster.clustersmilarity.IDecoyDiscriminator
  * User: Steve
  * Date: 1/24/14
+ *
+ * todo: development
  */
+@Deprecated
 public interface IDecoyDiscriminator {
 
 
