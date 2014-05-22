0d40ea36c0fbdc87a7f7a1c2e1490ff81219477a
harry.r.wang <harry.r.wang@gmail.com>
2014-05-22 15:38:48 +0000

    add assembly for exporter

--- spectra-cluster-hadoop/branches/development/script/runExport.sh
+++ spectra-cluster-hadoop/branches/development/script/runExport.sh
@@ -7,4 +7,4 @@ INPUT_PATH=$3
 
 ##### RUN it on the production LSF cluster
 ## this is not queued in the PRIDE LSF submission group, this is submitted as regular job as it is independent of any other job
-bsub -M 4000 -R "rusage[mem=4000]" -q production-rh6 -u pride.eb.test@gmail.com -J PRIDE-CLUSTER-EXPORT java -Xmx4000m -jar ${project.build.finalName}.jar ${OUTPUT_PATH} ${FILTER_XML} ${INPUT_PATH}
+bsub -M 4000 -R "rusage[mem=4000]" -q production-rh6 -u pride.eb.test@gmail.com -J PRIDE-CLUSTER-EXPORT java -Xmx4000m -jar spectra-cluster-hadoop-1.0-SNAPSHOT.jar ${OUTPUT_PATH} ${FILTER_XML} ${INPUT_PATH}
