447cc0d6e313a5bebcdc6bb3ee73c575d320a615
jgriss8 <jgriss8@yahoo.com>
2014-06-09 12:48:26 +0000

    Updated ConsensusSpectrumBuilderTest to explicitly test different filtering strategies.

--- spectra-cluster/trunk/src/test/java/uk/ac/ebi/pride/spectracluster/consensus/ConsensusSpectrumBuilderComparisonTests.java
+++ spectra-cluster/trunk/src/test/java/uk/ac/ebi/pride/spectracluster/consensus/ConsensusSpectrumBuilderComparisonTests.java
@@ -2,11 +2,16 @@ package uk.ac.ebi.pride.spectracluster.consensus;
 
 import junit.framework.Assert;
 import org.junit.Test;
+import uk.ac.ebi.pride.spectracluster.filter.IPeakFilter;
+import uk.ac.ebi.pride.spectracluster.filter.MaximialPeakFilter;
 import uk.ac.ebi.pride.spectracluster.similarity.AllPeaksDotProduct;
+import uk.ac.ebi.pride.spectracluster.spectrum.IPeak;
 import uk.ac.ebi.pride.spectracluster.spectrum.ISpectrum;
+import uk.ac.ebi.pride.spectracluster.spectrum.Spectrum;
 import uk.ac.ebi.pride.spectracluster.util.ClusteringTestUtilities;
 import uk.ac.ebi.pride.spectracluster.util.Defaults;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,5 +48,40 @@ public class ConsensusSpectrumBuilderComparisonTests {
 
         Assert.assertTrue("Consensus spectrum must be similar", similarity > 0.85);
 
+    }
+
+    @Test
+    public void testSpectraFiltering() throws Exception {
+        // filter the spectra first
+        IPeakFilter filter = new MaximialPeakFilter(100);
+
+        // load and filter the spectra
+        List<ISpectrum> spectra= ClusteringTestUtilities.readISpectraFromResource(SMALL_CLUSTER_FILE);
+        List<ISpectrum> filteredSpectra = new ArrayList<ISpectrum>(spectra.size());
+
+        for (ISpectrum s : spectra) {
+            List<IPeak> peaks = s.getPeaks();
+            List<IPeak> filteredPeaks = filter.filter(peaks);
+
+            Spectrum filteredSpectrum = new Spectrum(s, filteredPeaks);
+
+            filteredSpectra.add(filteredSpectrum);
+        }
+
+        // create the consensus spectra
+        final IConsensusSpectrumBuilder builder = Defaults.getDefaultConsensusSpectrumBuilder();
+
+        builder.addSpectra(spectra.toArray(new ISpectrum[spectra.size()]));
+        ISpectrum unfilteredConsensusSpectrum = builder.getConsensusSpectrum();
+
+        builder.clear();
+        builder.addSpectra(filteredSpectra.toArray(new ISpectrum[filteredSpectra.size()]));
+        ISpectrum filteredConsensusSpectrum = builder.getConsensusSpectrum();
+
+        // compare the spectra
+        final AllPeaksDotProduct allPeaksDotProduct = new AllPeaksDotProduct(Defaults.getSimilarityMZRange());
+
+        final double similarity = allPeaksDotProduct.assessSimilarity(unfilteredConsensusSpectrum, filteredConsensusSpectrum);
+        Assert.assertTrue("Consensus spectra must be similar", similarity >= 0.95);
     }
 }
