ce553ed13e1011cb1f32536b49c411bcc3fce35a
harry.r.wang@gmail.com <harry.r.wang@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2014-09-22 14:44:24 +0000

    enhenced shell script

--- spectra-cluster-importer/trunk/scripts/runAnnotator.sh
+++ spectra-cluster-importer/trunk/scripts/runAnnotator.sh
@@ -9,13 +9,13 @@ PROJECT_ACCESSION=$2
 
 ##### VARIABLES
 # the name to give to the LSF job (to be extended with additional info)
-JOB_NAME="PRIDE-CLUSTER-ANNOTATION"
-# memory limit
-MEMORY_LIMIT=15000
+JOB_NAME="PRIDE-CLUSTER-ANNOTATION-${PROJECT_ACCESSION}"
+# memory limit in MGb
+MEMORY_LIMIT=10000
 # LSF email notification
 JOB_EMAIL="rwang@ebi.ac.uk"
 
 
 ##### RUN it on the production LSF cluster #####
 ##### NOTE: you can change LSF group to modify the number of jobs can be run concurrently #####
-bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q production-rh6 -g /pride_cluster_annotation -u ${JOB_EMAIL} -J ${JOB_NAME} java -Xmx${MEMORY_LIMIT} -cp ${project.build.finalName}.jar uk.ac.ebi.pride.tools.cluster.annotator.ArchiveProjectAnnotator ${ARCHIVE_ROOT_FILE_PATH} ${PROJECT_ACCESSION}
+bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q production-rh6 -g /pride_cluster_annotation -u ${JOB_EMAIL} -J ${JOB_NAME} java -Xmx${MEMORY_LIMIT}m -cp ${project.build.finalName}.jar uk.ac.ebi.pride.tools.cluster.annotator.ArchiveProjectAnnotator ${ARCHIVE_ROOT_FILE_PATH} ${PROJECT_ACCESSION}
