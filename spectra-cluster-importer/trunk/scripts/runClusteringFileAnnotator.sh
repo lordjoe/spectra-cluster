2ca69bde152d773366da3820397bc5e691efdb40
harry.r.wang <harry.r.wang@gmail.com>
2014-09-30 12:28:20 +0000

    fixed a bug with writing modifications for multiple peptides, change shell script to write to log files instead of email

--- spectra-cluster-importer/trunk/scripts/runClusteringFileAnnotator.sh
+++ spectra-cluster-importer/trunk/scripts/runClusteringFileAnnotator.sh
@@ -3,6 +3,7 @@
 ##### OPTIONS
 # (required)  path to .clustering file
 CLUSTER_FILE_PATH=$1
+CLUSTER_FILE_NAME=$(basename "$CLUSTER_FILE_PATH" ".clustering")
 OUTPUT_FILE_PATH=$2
 
 
@@ -11,10 +12,10 @@ OUTPUT_FILE_PATH=$2
 JOB_NAME="PRIDE-CLUSTERING-FILE-ANNOTATOR"
 # memory limit in MGb
 MEMORY_LIMIT=10000
-# LSF email notification
-JOB_EMAIL="rwang@ebi.ac.uk"
+# log file name
+LOG_FILE_NAME="${JOB_NAME}-${CLUSTER_FILE_NAME}"
 
 
 ##### RUN it on the production LSF cluster #####
 ##### NOTE: you can change LSF group to modify the number of jobs can be run concurrently #####
-bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q production-rh6 -g /pride_clustering_file_annotator -u ${JOB_EMAIL} -J ${JOB_NAME} java -Xmx${MEMORY_LIMIT}m -cp ${project.build.finalName}.jar uk.ac.ebi.pride.tools.cluster.exporter.ClusteringFileAnnotator ${CLUSTER_FILE_PATH} ${OUTPUT_FILE_PATH}
+bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q production-rh6 -g /pride_clustering_file_annotator -o /dev/null -J ${JOB_NAME} ./runInJava.sh ./log/${LOG_FILE_NAME}.log ${MEMORY_LIMIT}m -cp ${project.build.finalName}.jar uk.ac.ebi.pride.tools.cluster.exporter.ClusteringFileAnnotator ${CLUSTER_FILE_PATH} ${OUTPUT_FILE_PATH}
