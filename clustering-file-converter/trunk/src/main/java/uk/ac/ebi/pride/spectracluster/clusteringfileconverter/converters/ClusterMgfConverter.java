97ef358204c28939c0152d546c17ffd07ea42a08
jgriss8 <jgriss8@yahoo.com>
2014-12-02 07:41:00 +0000

    jg - Updated clustering-file-converter to ignore empty peaks and output a warning.

--- clustering-file-converter/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/clusteringfileconverter/converters/ClusterMgfConverter.java
+++ clustering-file-converter/trunk/src/main/java/uk/ac/ebi/pride/spectracluster/clusteringfileconverter/converters/ClusterMgfConverter.java
@@ -27,13 +27,23 @@ public class ClusterMgfConverter extends AbstractClusterConverter {
     public String convertCluster(ICluster cluster) {
         StringBuilder stringBuilder = new StringBuilder("BEGIN IONS\n");
 
-        stringBuilder.append(String.format("TITLE=%s (consensus spectrum %d)\n", cluster.getMaxSequence(), clusterCounter));
+        stringBuilder.append(String.format("TITLE=%s\n", (cluster.getId() != null) ? cluster.getId() : cluster.getMaxSequence(), clusterCounter));
         int charge = ClusterUtilities.calculateClusterCharge(cluster);
         stringBuilder.append(String.format("PEPMASS=%.3f\n", cluster.getAvPrecursorMz()));
         stringBuilder.append(String.format("CHARGE=%d%c\n", Math.abs(charge), (charge > 0 ? '+' : '-')));
 
         // add the peak list
         for (int i = 0; i < cluster.getConsensusMzValues().size(); i++) {
+            // ignore peaks with 0 m/z and 0 intensity
+            if (cluster.getConsensusMzValues().get(i) == 0) {
+                System.out.println("Warning: Cluster " + cluster.getId() + " contains empty peak (m/z).");
+                continue;
+            }
+            if (cluster.getConsensusIntensValues().get(i) == 0) {
+                System.out.println("Warning: Cluster " + cluster.getId() + " contains empty peak (intensity).");
+                continue;
+            }
+
             stringBuilder.append(cluster.getConsensusMzValues().get(i)).append(" ").append(cluster.getConsensusIntensValues().get(i)).append("\n");
         }
 
